fdb34013f9f0048a8fea1212e8c05a305feb10e1
Owen Zhang <owenzhang@owendemacbook.attlocal.net>
2019-09-28 15:43:07 -0500

    feat: disable buttons with time conflicts

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ const Banner = ({ title }) => (
   <Title>{ title }</Title>
 );
 
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+
+const getCourseTerm = course => (
+  terms[course.id.charAt(0)]
+);
+
+const getCourseNumber = course => (
+  course.id.slice(1, 4)
+)
+  
+
 const CourseList = ({ courses }) => {
   const [term, setTerm] = useState('Fall');
   const [selected, toggle] = useSelection();
@@ -23,6 +34,56 @@ const CourseList = ({ courses }) => {
     </React.Fragment>
   );
 };
+
+// a conflict must involve overlapping days and times
+const days = ['M', 'Tu', 'W', 'Th', 'F'];
+
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+
+const timeParts = meets => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match ? {} : {
+    days,
+    hours: {
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
+};
+
+const daysOverlap = (days1, days2) => ( 
+  days.some(day => days1.includes(day) && days2.includes(day))
+);
+
+const hoursOverlap = (hours1, hours2) => (
+  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
+);
+
+const timeConflict = (course1, course2) => (
+  daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
+);
+
+const courseConflict = (course1, course2) => (
+  course1 !== course2
+  && getCourseTerm(course1) === getCourseTerm(course2)
+  && timeConflict(course1, course2)
+);
+
+const hasConflict = (course, selected) => (
+  selected.some(selection => courseConflict(course, selection))
+);
+
+const addCourseTimes = course => ({
+  ...course,
+  ...timeParts(course.meets)
+});
+
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: schedule.courses.map(addCourseTimes)
+});
+
 const useSelection = () => {
   const [selected, setSelected] = useState([]);
   const toggle = (x) => {
@@ -49,20 +110,13 @@ const TermSelector = ({ state }) => (
   </Button.Group>
 );
 
-const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
-
-const getCourseTerm = course => (
-  terms[course.id.charAt(0)]
-);
-
-const getCourseNumber = course => (
-  course.id.slice(1, 4)
-)
-  
-const Course = ({ course }) => (
-  <button className="button">
+const Course = ({ course, state }) => (
+  <Button color={ buttonColor(state.selected.includes(course)) }
+    onClick={ () => state.toggle(course) }
+    disabled={ hasConflict(course, state.selected) }
+    >
     { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
-  </button>
+  </Button>
 );
 
 
@@ -75,7 +129,7 @@ const App = () => {
       const response = await fetch(url);
       if (!response.ok) throw response;
       const json = await response.json();
-      setSchedule(json);
+      setSchedule(addScheduleTimes(json));
     }
     fetchSchedule();
   }, [])
